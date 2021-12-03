b936cd21ccd6f8ad5d4862b9714d01154eb3dd03
huntakev <huntakev@gmail.com>
2021-12-02 20:00:44 -0800

    Moved files, fixed docstrings, added missing brackets

--- Day 1/day_1_aoc21.py
+++ Day 1/day_1_aoc21.py
@@ -1,38 +1,42 @@
-FILE = "C:/Users/hunta/Desktop/data.txt.txt"
-x = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+FILE = "day_1_aoc21.txt"
+SAMPLE_INPUT = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
-def txt_to_list(dir_file: str):
-    '''
+
+def unpack_puzzle_input(dir_file: str) -> list:
+    """
     Args:
-        dir_file (str): Location of file to pull data from
+        dir_file (str): location of .txt file to pull data from
     Returns:
         list of numeric values saved in .txt file
-    '''
+    """
     with open(dir_file, "r") as file:
         content = file.read()
         data = content.split("\n")
     return list(map(int, data))
 
-def count_sequential_elem_increases(arr: list):
-    '''
+
+def count_sequential_elem_increases(arr: list) -> int:
+    """
     Args:
-        arr (list): List of numeric values
+        arr (list): list of numeric values
     Returns:
         count of the number of times that a value increases from the previous array entry
-    '''
+    """
     return sum(arr[i-1] < arr[i] for i in range(1, len(arr)))
 
-def count_sequential_window_increases(arr: list):
-    '''
+
+def count_sequential_window_increases(arr: list) -> int:
+    """
     Args:
-        arr (list): List of numeric values
+        arr (list): list of numeric values
     Returns:
         count of number of times that the sum of a rolling 3 element window is larger than
         the previous adjacent 3 element window
-    '''
+    """
     return sum(sum(arr[i-4:i-1]) < sum(arr[i-3:i])
                for i in range(3, len(arr)))
 
-data = txt_to_list(FILE)
-print(count_sequential_elem_increases(data)
-print(count_sequential_window_increases(data)
+
+puzzle_input = unpack_puzzle_input(FILE)
+print(count_sequential_elem_increases(puzzle_input))
+print(count_sequential_window_increases(puzzle_input))
